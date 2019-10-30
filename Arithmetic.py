829b9eaa8abc6e1df6295d0888a13b5148b8a435
edenvvv <57102615+edenvvv@users.noreply.github.com>
2019-10-30 14:12:24 +0200

    Create Arithmetic.py

---
+++ Arithmetic.py
@@ -0,0 +1,38 @@
+"""
+The function adds two numbers
+"""
+
+
+def add(num1, num2):
+    result = num1 + num2
+    return result
+
+
+"""
+The function subtracts two numbers
+"""
+
+
+def sub(num1, num2):
+    result = num1 - num2
+    return result
+
+
+"""
+The function multiplies two numbers
+"""
+
+
+def mul(num1, num2):
+    result = num1 * num2
+    return result
+
+
+"""
+The function divides two numbers
+"""
+
+
+def div(num1, num2):
+    result = num1 / num2
+    return result
